cf9aaa9d97c4540780d4ebb07c6e8398e2c66ace
Pervaz11 <parvazsa@code.edu.az>
2025-02-21 01:30:11 +0400

    Upcoming section design

---
+++ src/assets/js/helpers/slider.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper('.swiper', {
+    slidesPerView: 3, 
+    spaceBetween: 30,
+    loop: true,
+
+    direction: getDirection(),
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    on: {
+        resize: function () {
+            swiper.changeDirection(getDirection());
+        },
+    },
+});
+
+function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+    return direction;
+}
